b2eb562f11cc2bcf31f3fc3f873688f563eece31
DESKTOP-I9SF0ON\User <gargarja@students.nu-clark.edu.ph>
2024-10-10 10:58:17 +0800

    activity9999

---
+++ activity9.java
@@ -0,0 +1,12 @@
+public class activity9{
+    public static void main (String [] args){
+        int age = 20;
+        if (age > 18) {
+            System.out.print("Person is an Adult");
+        }
+        else{
+            System.out.print("Person is a kid");
+        }
+        
+    }
+}
